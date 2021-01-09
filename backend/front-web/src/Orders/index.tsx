3204e28e29ba91d8b6253bba63e2825903adbdf3
souzaoe <souzaoe@hotmail.com>
2021-01-08 22:40:49 -0300

    Implementando pagina de escolha de produtos

--- backend/front-web/src/Orders/index.tsx
+++ backend/front-web/src/Orders/index.tsx
@@ -1,10 +1,27 @@
-import StepsHeader from './StepsHeader';
+import { useEffect, useState } from 'react';
 import './styles.css';
+import { fetchProducts } from '../api';
+import { Product } from './types';
+import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+
 
 function Orders(){
+
+    const [products, setProducts] = useState<Product[]>([]);
+    
+
+    useEffect(() => {
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(erro => console.log(erro))
+    }, []); 
+
+    
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products}/>
         </div>    
     )
 }
